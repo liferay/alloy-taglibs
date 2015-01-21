600afb05bbf5e253bec06e4cddf0d92bb3e71f21
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-01-21 11:00:53 +0100

    LPS-52071 Regen

--- alloy-taglib/src/com/liferay/alloy/taglib/alloy/base/BaseOverlayContextTag.java
+++ alloy-taglib/src/com/liferay/alloy/taglib/alloy/base/BaseOverlayContextTag.java
@@ -23,7 +23,7 @@ import javax.servlet.jsp.JspException;
  * @author Nathan Cavanaugh
  * @generated
  */
-public class BaseOverlayContextTag extends com.liferay.taglib.util.IncludeTag {
+public abstract class BaseOverlayContextTag extends com.liferay.taglib.util.IncludeTag {
 
 	@Override
 	public int doStartTag() throws JspException {
