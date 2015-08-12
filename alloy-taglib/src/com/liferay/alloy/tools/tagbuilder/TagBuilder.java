23292b39f53c9e6888cc6505fe6342169fe0aa3f
Renato Rego <renato.rego@liferay.com>
2015-08-12 15:09:34 -0300

    AUI-1955 - Read attributes "writeEndJSP" and "writeStartJSP" from XML file used to generate a taglib and set them to the taglib components.

--- alloy-taglib/src/com/liferay/alloy/tools/tagbuilder/TagBuilder.java
+++ alloy-taglib/src/com/liferay/alloy/tools/tagbuilder/TagBuilder.java
@@ -283,8 +283,14 @@ public class TagBuilder {
 			String parentClass = GetterUtil.getString(
 				node.attributeValue("parentClass"), _DEFAULT_PARENT_CLASS);
 
+			boolean writeEndJSP = GetterUtil.getBoolean(
+				node.attributeValue("writeEndJSP"), true);
+
 			boolean writeJSP = GetterUtil.getBoolean(
 				node.attributeValue("writeJSP"), true);
+
+			boolean writeStartJSP = GetterUtil.getBoolean(
+				node.attributeValue("writeStartJSP"), true);
 
 			String[] authors = getAuthorList(node);
 			List<Attribute> attributes = getAttributes(node);
@@ -304,7 +310,9 @@ public class TagBuilder {
 			component.setName(name);
 			component.setPackage(componentPackage);
 			component.setParentClass(parentClass);
+			component.setWriteEndJSP(writeEndJSP);
 			component.setWriteJSP(writeJSP);
+			component.setWriteStartJSP(writeStartJSP);
 
 			components.add(component);
 		}
